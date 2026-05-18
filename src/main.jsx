a6a176bc5caec651416faae8ac1c9137e5019bc1
Tomoriu Akarin <lequocanh10000@github.com>
2026-05-18 21:36:33 +0700

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom' // Thêm dòng này
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter> {/* Bọc App ở đây */}
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
 )
