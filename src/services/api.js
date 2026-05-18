c1850d33187a13e4eb0fc05538ea0fd0bd3a8191
Tomoriu Akarin <lequocanh10000@github.com>
2026-05-18 20:49:37 +0700

    finish

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://be-7bv6.onrender.com/api/v1'
 });
 
 api.interceptors.request.use((config) => {
