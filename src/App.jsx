e5170f2a6d8f12b16f3e760f9e9d9ad94a728639
Đặng Sỹ Toàn <22024575@vnu.edu.vn>
2026-05-19 11:05:22 +0700

    ux: automatically scroll window to top on page route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AppRoutes from './routes';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App min-h-screen flex flex-col">
+      <ScrollToTop />
       <div className="flex-1 flex flex-col">
         <AppRoutes />
       </div>
